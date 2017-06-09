3d7fd2c385766e337415129ddd215861ad24ca58
cyberblast <git@cyberblast.org>
2017-06-09 21:01:42 +0200

    Update Assembly Info

--- Notify/Properties/AssemblyInfo.cs
+++ Notify/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Notify")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("https://github.com/cyberblast/Screeps.Notify")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Notify")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Robert Rakowitz 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
